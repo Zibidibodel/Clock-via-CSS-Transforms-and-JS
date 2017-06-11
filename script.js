11e20265da208a904120788a74a466b928d97307
fourmatsee <zibidibodel@gmail.com>
2017-06-11 01:05:22 -0700

    removed console logs from testing

--- script.js
+++ script.js
@@ -16,9 +16,6 @@ function setDate() {
     const hours = now.getHours();
     const hoursDegrees = ((hours/12) * 360) + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-
-    console.log (hours);
-    console.log (hoursDegrees);
 }
 
 setInterval(setDate, 1000);
